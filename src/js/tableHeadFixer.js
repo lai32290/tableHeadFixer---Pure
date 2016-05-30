2da8bf658b3841d1c6941cd8e4c023303ec75ae1
Lai <lai32690@gmail.com>
2016-05-30 16:16:05 -0300

    update structure

--- src/js/tableHeadFixer.js
+++ src/js/tableHeadFixer.js
@@ -1,30 +1,34 @@
 (function ()
 {
+    'use strict';
+    window.tableHeadFixer = 'tableHeadFixer' = Date.now();
+
     HTMLTableElement.prototype.tableHeadFixer = function (props)
     {
+        var u        = Utils;
         var self     = this;
-        self.default = {
-            top: true
-            , bottom: false
-            , left: 0
-            , right: 0
-            , 'z-index': 100
+        self.config  = {
+            default : {
+                top: true
+                , bottom: false
+                , left: 0
+                , right: 0
+                , 'z-index': 100
+            }
         };
-
 
     };
 
-    function extend()
-    {
-        try
-        {
-            var object = arguments[0];
+    function Utils(){}
+    Utils.prototype.extend = function(target) {
+        var sources = [].slice.call(arguments, 1);
 
-            
-        }
-        catch (e)
-        {
-            console.log(e);
-        }
-    }
+        sources.forEach(function(source) {
+            for(var prop in source) {
+                target[prop] = source[prop];
+            }
+        });
+
+        return target;
+    };
 })();
