be59629b61478ee251c934a0fb75bbdfd9890042
Lai <lai32690@gmail.com>
2016-05-30 16:34:55 -0300

    update structure

--- src/js/tableHeadFixer.js
+++ src/js/tableHeadFixer.js
@@ -1,10 +1,9 @@
 (function ()
 {
     'use strict';
-    window.tableHeadFixer = 'tableHeadFixer' = Date.now();
+    HTMLTableElement.prototype.tableHeadFixer = tableHeadFixer;
 
-    HTMLTableElement.prototype.tableHeadFixer = function (props)
-    {
+    function tableHeadFixer(props) {
         var u        = Utils;
         var self     = this;
         self.config  = {
@@ -17,18 +16,42 @@
             }
         };
 
-    };
+        for(var k in []) {
+            Data(ele, 'k', 'sim');
+        }
+    }
+    tableHeadFixer.id = 'tableHeadFixer' = Date.now();
 
     function Utils(){}
-    Utils.prototype.extend = function(target) {
-        var sources = [].slice.call(arguments, 1);
+    Utils.prototype = {
+        extend: function(target) {
+            var sources = [].slice.call(arguments, 1);
 
-        sources.forEach(function(source) {
-            for(var prop in source) {
-                target[prop] = source[prop];
-            }
-        });
+            sources.forEach(function(source) {
+                for(var prop in source) {
+                    target[prop] = source[prop];
+                }
+            });
 
-        return target;
+            return target;
+        }
     };
+
+    /* 
+     * owner = HTML element
+     * key = string
+     * value = anything
+     */
+    function Data(owner, key, value) {
+        var prop = owner[tableHeadFixer.id];
+
+        if(!prop) {
+            prop = {};
+            prop[key] = value;
+
+            owner[tableHeadFixer.id] = prop;
+        }
+
+        return prop;
+    }
 })();
